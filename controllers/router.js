e7a1161c0b76520ce6678dc0251414a50f042884
Tran Nam <nam.tran@unicorn.vn>
2018-05-29 11:44:32 +0700

    add get UserInfo and list User

--- controllers/router.js
+++ controllers/router.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var Status = require('../controllers/statusController');
+var Users = require('../controllers/UserController');
 
 var apiRoutes = express.Router();
   
 apiRoutes.route('/status').get(Status.list);
 apiRoutes.route('/status').post(Status.post);
+apiRoutes.route('/userinfo').get(Users.userInfo);
+apiRoutes.route('/listuser').get(Users.listUser);
 
 module.exports = apiRoutes;
