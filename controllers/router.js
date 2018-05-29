cdeee06ddc7104862c37a61ed3e3e3be921a4e2d
Tran Nam <nam.tran@unicorn.vn>
2018-05-29 12:23:49 +0700

    update api get userinfo

--- controllers/router.js
+++ controllers/router.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var Status = require('../controllers/statusController');
-var Users = require('../controllers/UserController');
+var User = require('../controllers/UserController');
 
 var apiRoutes = express.Router();
   
 apiRoutes.route('/status').get(Status.list);
 apiRoutes.route('/status').post(Status.post);
-apiRoutes.route('/userinfo').get(Users.userInfo);
-apiRoutes.route('/listuser').get(Users.listUser);
+apiRoutes.route('/users').get(User.listUser);
+apiRoutes.route('/users/:userId').get(User.userInfo);
 
 module.exports = apiRoutes;
