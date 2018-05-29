de6c867752c1087d15b3a9800018978fa2643b61
Andy Ngo <34905850+andyngo95@users.noreply.github.com>
2018-05-29 14:26:21 +0700

    Update getStatusByUserId

--- controllers/router.js
+++ controllers/router.js
@@ -6,13 +6,14 @@ var Contact = require('../controllers/contactController')
 
 var apiRoutes = express.Router();
   
-apiRoutes.route('/status').get(Status.list);
-apiRoutes.route('/status').post(Status.post);
 apiRoutes.route('/login').post(User.authenticate);
+
 apiRoutes.route('/statuses').get(Status.list);
 apiRoutes.route('/statuses').post(Status.post);
+apiRoutes.route('/statuses/:userId').get(Status.listByUserId);
+
 apiRoutes.route('/users').get(User.listUser);
 apiRoutes.route('/users/:userId').get(User.userInfo);
 
-apiRoutes.route('/contact').get(Contact.list);
+apiRoutes.route('/contacts').get(Contact.list);
 module.exports = apiRoutes;
